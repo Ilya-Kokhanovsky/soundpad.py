ac4445aa73be08e86e50a50f0dd8e17c594da47a
Ilya Kokhanovsky <114568934+Ilya-Kokhanovsky@users.noreply.github.com>
2024-10-26 04:08:09 +0300

    Update pyproject.toml

--- soundpad_control/__init__.py
+++ soundpad_control/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'soundpad_control'
 __author__ = 'Ilya Kokhanovsky'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2024-present Ilya Kokhanovsky'
-__version__ = '0.1.0'
+__version__ = '0.1.2'
 
 from .remote_control import SoundpadRemoteControl
 
